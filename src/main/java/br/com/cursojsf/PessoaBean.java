bb2d0c690ad786a7e2277a5eb86dfc5d505ce48d
MuriloMarquesSantos <murilommms@gmail.com>
2018-09-07 18:37:35 -0300

    Adicionando atributo nomeCompleto

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -2,23 +2,39 @@ package br.com.cursojsf;
 
 import javax.faces.bean.ManagedBean;
 
-@ManagedBean(name="pessoaBean")
+@ManagedBean(name = "pessoaBean")
 public class PessoaBean {
 
-	private String nome = "Murilo";
+	private String nome;
 	private String sobrenome;
+	private String nomeCompleto;
+	
+	public void mostrarNome(){
+		nomeCompleto = nome + " " + sobrenome;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
-	
-	
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
 }
