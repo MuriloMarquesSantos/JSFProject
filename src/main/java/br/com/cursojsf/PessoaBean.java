746e30575d576182e015286a502857c926d18136
MuriloMarquesSantos <murilommms@gmail.com>
2018-09-10 10:38:32 -0300

    Adicionando lista de nomes e alterando para ViewScoped para manter o bean após finalização da request

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,18 +1,31 @@
 package br.com.cursojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
+@ViewScoped
 @ManagedBean(name = "pessoaBean")
-@RequestScoped
 public class PessoaBean {
 
 	private String nome;
-	private String sobrenome;
-	private String nomeCompleto;
-	
-	public void addNome(){
-		nomeCompleto = nome + " " + sobrenome;
+
+	private List<String> nomes = new ArrayList<String>();
+
+	public String addNome() {
+		nomes.add(nome);
+		return "";
+	}
+
+	public List<String> getNomes() {
+		return nomes;
+	}
+
+	public void setNomes(List<String> nomes) {
+		this.nomes = nomes;
 	}
 
 	public String getNome() {
@@ -21,22 +34,6 @@ public class PessoaBean {
 
 	public void setNome(String nome) {
 		this.nome = nome;
-	}
-
-	public String getSobrenome() {
-		return sobrenome;
-	}
-
-	public void setSobrenome(String sobrenome) {
-		this.sobrenome = sobrenome;
-	}
-
-	public String getNomeCompleto() {
-		return nomeCompleto;
-	}
-
-	public void setNomeCompleto(String nomeCompleto) {
-		this.nomeCompleto = nomeCompleto;
 	}
 
 }
