2f572d2d4c5d996b7cdff5038d4834a3dfcd33b8
MuriloMarquesSantos <murilommms@gmail.com>
2018-09-10 10:51:55 -0300

    Adicionando backing bone

--- src/main/java/br/com/cursojsf/PessoaBean.java
+++ src/main/java/br/com/cursojsf/PessoaBean.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.html.HtmlCommandButton;
 
 @ViewScoped
 @ManagedBean(name = "pessoaBean")
@@ -14,9 +14,25 @@ public class PessoaBean {
 	private String nome;
 
 	private List<String> nomes = new ArrayList<String>();
+	
+	private HtmlCommandButton commandButton;
 
+	
+	public HtmlCommandButton getCommandButton(){
+		return this.commandButton;
+	}
+	
+	public void setCommandButton(HtmlCommandButton commandButton){
+		this.commandButton = commandButton;
+	}
+	
 	public String addNome() {
 		nomes.add(nome);
+		
+		if (nomes.size() > 3){
+			commandButton.setDisabled(true);
+		}
+		
 		return "";
 	}
 
